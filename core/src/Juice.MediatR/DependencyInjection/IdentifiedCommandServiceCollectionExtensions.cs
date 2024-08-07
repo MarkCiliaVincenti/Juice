a873b06a8adae817de4ed160c09d1e5f5262b8cc
ductran <ductran.tb@gmail.com>
2024-08-07 13:52:59 +0700

    Add AddIdentifiedCommandHandler

---
+++ core/src/Juice.MediatR/DependencyInjection/IdentifiedCommandServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Juice.MediatR;
+using MediatR;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class IdentifiedCommandServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIdentifiedCommandHandler<TRequest, THandler, TIdentifiedHandler>(this IServiceCollection services)
+            where TRequest : IRequest
+            where THandler : class, IRequestHandler<TRequest>
+            where TIdentifiedHandler : class, IRequestHandler<IdentifiedCommand<TRequest>>
+        {
+            services.AddTransient<IRequestHandler<TRequest>, THandler>();
+            services.AddTransient<IRequestHandler<IdentifiedCommand<TRequest>>, TIdentifiedHandler>();
+            return services;
+        }
+
+        public static IServiceCollection AddIdentifiedCommandHandler<TRequest, TResponse, THandler, TIdentifiedHandler>(this IServiceCollection services)
+            where TRequest : IRequest<TResponse>
+            where THandler : class, IRequestHandler<TRequest, TResponse>
+            where TIdentifiedHandler : class, IRequestHandler<IdentifiedCommand<TRequest, TResponse>, TResponse>
+        {
+            services.AddTransient<IRequestHandler<TRequest, TResponse>, THandler>();
+            services.AddTransient<IRequestHandler<IdentifiedCommand<TRequest, TResponse>, TResponse>, TIdentifiedHandler>();
+            return services;
+        }
+    }
+}
