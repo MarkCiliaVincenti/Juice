b5b7a828065e1eb15849f395be1435db4a51e176
ductran <ductran.tb@gmail.com>
2024-09-06 14:37:48 +0700

    Add tenant test helper

--- core/test/Juice.EF.Tests/MultitenantDbContextTest.cs
+++ core/test/Juice.EF.Tests/MultitenantDbContextTest.cs
@@ -1,18 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Security.Claims;
-using System.Threading;
 using System.Threading.Tasks;
-using Finbuckle.MultiTenant;
 using Finbuckle.MultiTenant.Abstractions;
-using Finbuckle.MultiTenant.AspNetCore.Internal;
 using FluentAssertions;
+using Juice.Domain;
 using Juice.EF.Tests.Infrastructure;
 using Juice.Extensions.DependencyInjection;
+using Juice.Multitenant.SharedTest;
+using Juice.MultiTenant;
 using Juice.XUnit;
-using Juice.Services;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -53,14 +48,8 @@ namespace Juice.EF.Tests
                         options.MigrationsHistoryTable("__EFTestMigrationsHistory", "Contents");
                     });
                 });
-                services
-                    .AddMultiTenant<TenantInfo>()
-                    .WithInMemoryStore(options =>
-                    {
-                        options.Tenants.Add(new TenantInfo { Id = "tenant-1", Identifier = "tenant-1" });
-                    })
-                    .WithStaticStrategy("tenant-1");
-                ;
+                services.AddTenantTest<TenantInfo>();
+
                 services.AddMediatR(options =>
                 {
                     options.RegisterServicesFromAssemblyContaining(typeof(EFTest));
@@ -78,47 +67,25 @@ namespace Juice.EF.Tests
             });
 
             var serviceProvider = resolver.ServiceProvider.CreateScope().ServiceProvider;
-            HttpContext httpContext = new MyHttpContext(serviceProvider);
-            var next = new RequestDelegate(async context =>
+            await TenantTestHelper.InnerTenantAsync(serviceProvider, context =>
             {
                 var tenantContextAccessor = serviceProvider.GetRequiredService<IMultiTenantContextAccessor>();
                 tenantContextAccessor.MultiTenantContext.Should().NotBeNull();
                 tenantContextAccessor.MultiTenantContext.TenantInfo.Should().NotBeNull();
                 var dbContext = serviceProvider.GetRequiredService<TestContext>();
                 dbContext.TenantInfo.Should().NotBeNull();
-                _output.WriteLine("dbContext.TenantInfo: {0}", dbContext.TenantInfo.Identifier);
+                _output.WriteLine("dbContext.TenantInfo: {0}", dbContext.TenantInfo!.Identifier);
+                return Task.CompletedTask;
             });
-            await (new MultiTenantMiddleware(next).Invoke(httpContext));
-            
+
         }
 
     }
-    internal class MyHttpContext : HttpContext
+
+    internal class TenantInfo : DynamicEntity<string>, ITenant, ITenantInfo
     {
-        private readonly IServiceProvider _serviceProvider;
+        public string? Identifier { get; set; }
 
-        public MyHttpContext(IServiceProvider serviceProvider)
-        {
-            _serviceProvider = serviceProvider;
-        }
-
-        public override IFeatureCollection Features => throw new NotImplementedException();
-
-        public override HttpRequest Request => throw new NotImplementedException();
-
-        public override HttpResponse Response => throw new NotImplementedException();
-
-        public override ConnectionInfo Connection => throw new NotImplementedException();
-
-        public override WebSocketManager WebSockets => throw new NotImplementedException();
-
-        public override ClaimsPrincipal User { get; set; }
-        public override IDictionary<object, object?> Items { get; set; } = new Dictionary<object, object?>();
-        public override IServiceProvider RequestServices { get => _serviceProvider; set => throw new NotImplementedException(); }
-        public override CancellationToken RequestAborted { get; set; }
-        public override string TraceIdentifier { get; set; } = new DefaultStringIdGenerator().GenerateUniqueId();
-        public override ISession Session { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public override void Abort() { }
-}
+        public string? OwnerUser { get; set; }
+    }
 }
---
+++ test/Juice.Multitenant.SharedTest/HttpContext.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Claims;
+using Juice.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace Juice.Multitenant.SharedTest
+{
+    internal class MyHttpContext : HttpContext
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private CancellationTokenSource _abortedCts = new CancellationTokenSource();
+        public MyHttpContext(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public override IFeatureCollection Features => throw new NotImplementedException();
+
+        public override HttpRequest Request => throw new NotImplementedException();
+
+        public override HttpResponse Response => throw new NotImplementedException();
+
+        public override ConnectionInfo Connection => throw new NotImplementedException();
+
+        public override WebSocketManager WebSockets => throw new NotImplementedException();
+
+        public override ClaimsPrincipal User { get; set; } = new ClaimsPrincipal();
+        public override IDictionary<object, object?> Items { get; set; } = new Dictionary<object, object?>();
+        public override IServiceProvider RequestServices { get => _serviceProvider; set => throw new NotImplementedException(); }
+        public override CancellationToken RequestAborted { get => _abortedCts.Token; set { _abortedCts = CancellationTokenSource.CreateLinkedTokenSource(_abortedCts.Token, value); } }
+        public override string TraceIdentifier { get; set; } = new DefaultStringIdGenerator().GenerateUniqueId();
+        public override ISession Session { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void Abort() => _abortedCts.Cancel();
+    }
+}
